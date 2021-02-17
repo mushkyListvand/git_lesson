7a475a7a5cf35f78df25bedb36823b277cee4943
mushkyListvand <79132329+mushkyListvand@users.noreply.github.com>
2021-02-17 11:56:06 +0200

    x->z

--- src/my_square.py
+++ src/my_square.py
@@ -1,11 +1,11 @@
 
 
 
-def my_square(x):
+def my_square(z):
         """takes a value and returns the squared value.
            uses the *  operator 
         """
-        return(x**2)
+        return(z**2)
 def my_square2(z):
         return(z*z))
 
